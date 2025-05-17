f7cc6183e04790dbada944e6f61cfb2c60ac2b0b
Ilia Butovskii <106578818+ilya-butovski@users.noreply.github.com>
2025-05-18 01:00:05 +0200

    task2.1

---
+++ src/microservices/proxy/index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const httpProxy = require('http-proxy');
+
+const MONOLITH_URL = process.env.MONOLITH_URL;
+const MOVIES_SERVICE_URL = process.env.MOVIES_SERVICE_URL;
+const FEATURE_ENABLED = process.env.GRADUAL_MIGRATION;
+const MOVIES_MIGRATION_PERCENT = process.env.MOVIES_MIGRATION_PERCENT;
+
+
+const config = [
+    {
+        startsWith: "/api/movies",
+        target: MOVIES_SERVICE_URL
+    }
+]
+
+
+
+const proxy = httpProxy.createProxyServer();
+
+const server = http.createServer((req, res) => {
+    const target = getTarget(req);
+    proxy.web(req, res, { target });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+function getTarget(req) {
+    const path = req.url;
+    const target = config.find(c => path.startsWith(c.startsWith));
+
+    return target && FEATURE_ENABLED ? target.target : MONOLITH_URL;
+}
